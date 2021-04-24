b4d587c535130468bc00219fef2fffd0125a12e0
shahazad08 <sk.shahazad@gmail.com>
2021-04-24 13:20:21 +0530

    Added: UC 1 Functionality is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,6 @@
 class EmployeeWage {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("Branch 1 Code is Added");
 	}
 }
