084eb4d10a0d605b956ac152d338f083c2a51488
shahazad08 <sk.shahazad@gmail.com>
2021-04-24 13:29:36 +0530

    Added: UC 2 code is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,6 @@ class EmployeeWage {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation");
 		System.out.println("Branch 1 Code is Added");
+		System.out.println("Branch 2 Code is Added");
 	}
 }
