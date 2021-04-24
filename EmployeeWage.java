2076fbb4bcedb83e281adad46396069fdf5b634e
shahazad08 <sk.shahazad@gmail.com>
2021-04-24 13:41:17 +0530

    Deleted:Unused Code is Deleted

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,7 @@ class EmployeeWage {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation");
 		System.out.println("Branch 1 Code is Added");
-<<<<<<< HEAD
 		System.out.println("Branch 2 Code is Added");
-=======
 		System.out.println("Remote FIle line is added");
->>>>>>> 5698fac5aac80c741712513ad86866df9db5391f
 	}
 }
