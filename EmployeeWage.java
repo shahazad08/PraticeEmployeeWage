5698fac5aac80c741712513ad86866df9db5391f
shahazad08 <45813580+shahazad08@users.noreply.github.com>
2021-04-24 13:27:27 +0530

    "Added:Some Line of Code is added"

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,6 @@ class EmployeeWage {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation");
 		System.out.println("Branch 1 Code is Added");
+		System.out.println("Remote FIle line is added");
 	}
 }
